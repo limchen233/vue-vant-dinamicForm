898766051acad3af74d6ce7b0c97c95cc04e929e
limchen233 <chenlim233@163.com>
2021-04-19 17:13:24 +0800

    修改eslint配置文件和settings.json配置

--- src/components/dynamicFormEle/DyText.vue
+++ src/components/dynamicFormEle/DyText.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-    <van-field v-model="text" label="文本" placeholder="请输入文本" clearable name="text" :rules="[{ required: true, message: '此项不能为空' }]" />
+    <van-field v-model="text" label="文本" placeholder="请输入文本" clearable name="text"
+      :rules="[{ required: true, message: '此项不能为空' }]" />
   </div>
 </template>
 <script>
 export default {
   data () {
     return {
-      name: 'text',
+      name: 'DyText',
       text: ''
     }
   }
 }
 </script>
 <style lang="scss">
-
 </style>
