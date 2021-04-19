d02bcb94f1a1d63f4552d694abce32c577bdddef
limchen233 <chenlim233@163.com>
2021-04-19 14:08:55 +0800

    添加 sass（注意版本） node-sass vant文本组件

---
+++ src/views/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="mainPage">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <DyText />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import DyText from '@/components/dynamicFormEle/DyText'
+
+export default {
+  name: 'Index',
+  components: {
+    DyText
+  },
+  methods: {
+
+  }
+}
+</script>
+<style lang="scss">
+.mainPage{
+  img{
+    width: 30px;
+    height: 30px;
+  }
+}
+</style>
