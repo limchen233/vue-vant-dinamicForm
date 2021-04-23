88ff9ef9e4c9ba1e33f6ac18c0b453ac4e603634
limchen233 <chenlim233@163.com>
2021-04-23 13:55:21 +0800

    update

--- src/components/DynamicFormItem.vue
+++ src/components/DynamicFormItem.vue
@@ -4,11 +4,11 @@ import DyRadio from './dynamicFormEle/DyRadio'
 import DySelect from './dynamicFormEle/DySelect'
 import DyCheckbox from './dynamicFormEle/DyCheckbox'
 import DyDate from './dynamicFormEle/DyDate'
-
+import DyUpload from './dynamicFormEle/DyUpload'
 export default {
 	name: 'DynamicFormItem',
 	components: {
-		DyText, DyRadio, DySelect, DyCheckbox, DyDate
+		DyText, DyRadio, DySelect, DyCheckbox, DyDate, DyUpload
 	},
 	props: {
 		item: {
@@ -36,6 +36,8 @@ export default {
 			return (<DyCheckbox item={item}></DyCheckbox>)
 		} else if (item.type === 'date') {
 			return (<DyDate item={item}></DyDate>)
+		} else if (item.type === 'upload') {
+			return (<DyUpload item={item}></DyUpload>)
 		} else {
 			return (<div style="font-size:16px;color:red;">未识别的组件类型：【{item.type}】</div>)
 		}
---
+++ src/components/dynamicFormEle/DyUpload.vue
@@ -0,0 +1,10 @@
+<template>
+	<div></div>
+</template>
+<script>
+import DyBase from './DyBase'
+export default {
+	name: 'DyUpload',
+	extends: DyBase
+}
+</script>
